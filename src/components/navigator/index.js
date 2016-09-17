6f76a8283f5dbb48b816bdd1ea88e1084b7ca828
Mandish Shah <shahmandish95@gmail.com>
2016-09-17 00:08:11 -0400

    Remove the Rangle logo from menu

--- src/components/navigator/index.js
+++ src/components/navigator/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-function Navigator({ children, ...props }) {
+function Navigator({ children, isVisible, ...props }) {
+  if (!isVisible) {
+    return null;
+  }
+
   return (
     <nav data-testid={ props.testid } className="flex items-center p1 bg-white border-bottom">
       { children }
@@ -10,6 +14,7 @@ function Navigator({ children, ...props }) {
 
 Navigator.propTypes = {
   children: React.PropTypes.node,
+  isVisible: React.PropTypes.bool,
   testid: React.PropTypes.string,
 };
 
