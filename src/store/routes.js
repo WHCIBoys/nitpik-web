4026d89cff7f333fbe962bec3eb0c0c23b76ea33
Mandish Shah <shahmandish95@gmail.com>
2016-09-17 13:14:02 -0400

    Add onEnter Profile Page for Branny

--- src/store/routes.js
+++ src/store/routes.js
@@ -5,10 +5,16 @@ import AboutPage from '../containers/about-page';
 import HomePage from '../containers/home-page';
 import UserProfile from '../containers/user-profile';
 
+const profileQuery = (nextState) => {
+  const { params: { profileId } } = nextState;
+  console.log(profileId);
+  // Dispatch the actions here
+};
+
 export default (
   <Route path="/" component={ App }>
     <IndexRoute component={ HomePage }/>
     <Route path="about" component={ AboutPage }/>
-    <Route path="profile/:profileId" component={ UserProfile }/>
+    <Route onEnter={profileQuery} path="profile/:profileId" component={ UserProfile }/>
   </Route>
 );
