2b9c5d9d6dc5e37e2cecd0c1026b3975e8cd7cfd
Mandish Shah <shahmandish95@gmail.com>
2016-09-17 18:02:43 -0400

    Move actions into a seperate constants file

--- src/components/nit-list/nit.js
+++ src/components/nit-list/nit.js
@@ -4,7 +4,7 @@ function Nit({ content }) {
   return (
     <div
       className="flex items-center mt2 p2 bg-gray border rounded"
-      style={{borderRadius: '14px', width: '100%'}}>
+      style={{borderRadius: '10px', width: '100%'}}>
       <span className="h4">{ content }</span>
     </div>
   );
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,58 +1,4 @@
-export const LOGIN_USER_PENDING = 'App/LOGIN_USER_PENDING';
-export const LOGIN_USER_SUCCESS = 'App/LOGIN_USER_SUCCESS';
-export const LOGIN_USER_ERROR = 'App/LOGIN_USER_ERROR';
-
-export const LOGOUT_USER = 'App/LOGOUT_USER';
-export const FORM_RESET = 'redux-form/RESET';
-
 export const FACEBOOK_GRAPH_URL = 'https://graph.facebook.com/';
-
-export const MOCK_NITS_FOR_MAAZ = {
-  nits: [
-    {
-      id: 1,
-      content: 'Sup brandon',
-      author: {
-        name: 'Maaz',
-      },
-      user: {
-        name: 'Brandon',
-      },
-    },
-  ],
-  friendships: [
-    {
-      id: 1,
-      user: {
-        name: 'Maaz',
-      },
-      friend: {
-        name: 'Brandon Bowen',
-        facebookId: '10205270670229481',
-      },
-    },
-    {
-      id: 2,
-      user: {
-        name: 'Maaz',
-      },
-      friend: {
-        name: 'Mandish Shah',
-        facebookId: '10207412737774454',
-      },
-    },
-    {
-      id: 3,
-      user: {
-        name: 'Maaz',
-      },
-      friend: {
-        name: 'Ramanpreet Nara',
-        facebookId: '703342513133853',
-      },
-    },
-  ],
-};
 
 export const FAV_FRIENDS_LIST = [{
   firstName: 'Mandish',
@@ -68,8 +14,4 @@ export const FAV_FRIENDS_LIST = [{
   lastName: 'Bowen',
 }];
 
-export const LOAD_USER_NITS = {
-  PENDING: 'LOAD_USER_NITS_PENDING',
-  SUCCESS: 'LOAD_USER_NITS_SUCCESS',
-  ERROR: 'LOAD_USER_NITS_ERROR',
-};
+export * from './actions';
